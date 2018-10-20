5074b76d4828830ab1da641ceca7b997507ab3d6
Hasm95 <44320711+Hasm95@users.noreply.github.com>
2018-10-20 18:16:50 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once("db.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="styles/main.css" />
+</head>
+
+<body>
+ <center>
+   <img src="images/tiwar.jpg" />
+ </center>
+<hr color="#f4d06e"/>
+    <a id="reed" href="reg.php">
+       <img src="images/arena.png" alt>
+       Регистрация
+    </a>
+<hr color="#f4d06e"/>
+    <a id="reed" href="">
+       <img src="images/duel.png" alt>
+       Вход
+	</a>
+<hr color="#f4d06e"/>
+   <a id="reed" href="">
+       <img src="images/quest.png" alt>
+       Форум
+   </a> 
+<hr color="#f4d06e"/>
+ 
+</body>
+
+</html>
